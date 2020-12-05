4393b0304b95a358b721533365a10168848d6563
Ricardo Sequeira <ricardoseq@gmail.com>
2020-12-05 06:30:19 +0000

    day 5

--- day5/Program.cs
+++ day5/Program.cs
@@ -9,67 +9,113 @@ namespace adventofcode2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 1 of AoC");
-            IEnumerable<char[]> data = null;
-            Tools.Tools.MeasureActionTime("Load", () => {
+            Console.WriteLine("Day 5 of AoC");
+            IEnumerable<Tuple<string, int, int, int>> data = null;
+            Tools.Tools.MeasureActionTime("Load", () =>
+            {
                 string input = args[0];
                 Console.WriteLine($"Loading data from " + input + "...");
                 var raw = Tools.Tools.ReadAllLines(input);
                 data = LoadData(raw);
                 Console.WriteLine($"Length -> {data.Count()}");
             });
-            Tools.Tools.MeasureActionTime("Part 1", () => {
-                var res = DoMagic(data.ToArray());
+            Tools.Tools.MeasureActionTime("Part 1", () =>
+            {
+                var res = DoMagic(data);
                 Console.WriteLine($"Result -> {res}");
             });
-            Tools.Tools.MeasureActionTime("Part 2", () => {
-                var res = DoMagic2(data.ToArray());
+            Tools.Tools.MeasureActionTime("Part 2", () =>
+            {
+                var res = DoMagic2(data);
                 Console.WriteLine($"Result -> {res}");
             });
             Console.WriteLine("END");
         }
 
-        static IEnumerable<char[]> LoadData(IEnumerable<string> input)
+        static IEnumerable<Tuple<string, int, int, int>> LoadData(IEnumerable<string> input)
         {
             foreach (var item in input)
             {
-                yield return item.ToCharArray();
+                var row = getBSP(item.Substring(0, 7), 127);
+                var seat = getBSP(item.Substring(7, 3), 7);
+                yield return new Tuple<string, int, int, int>(item, row, seat, row * 8 + seat);
             }
         }
 
-        static int DoMagic(char[][] input, int xStep = 3, int yStep = 1)
+        static int getBSP(string input, int size)
         {
-                        
-            int x = 0, y = 0, res = 0;
-            while(true)
+            int rowMin = 0, rowMax = size;
+
+            foreach (var item in input)
             {
-                x = x + xStep;
-                y = y + yStep;                
-
-                if(y >= input.Count()) {
-                    break;
-                } 
-
-                if(x >= input[y].Count()){
-                    x = x - input[y].Count();
+                int inc = (rowMax - rowMin + 1) / 2;
+                if (item == 'F' || item == 'L')
+                {
+                    rowMax -= inc;
                 }
-
-                if(input[y][x] == '#'){
-                    res++;
+                if (item == 'B' || item == 'R')
+                {
+                    rowMin += inc;
                 }
             }
-            return res;
+            if (rowMin == rowMax)
+            {
+                return rowMin;
+            }
+            Console.WriteLine($"Error not found {input}|{rowMin}|{rowMax}");
+            return -1;
         }
 
-        static double DoMagic2(char[][] input)
+        static int DoMagic(IEnumerable<Tuple<string, int, int, int>> input)
         {
-            var test1 = DoMagic(input, 1, 1);
-            var test2 = DoMagic(input, 3, 1);
-            var test3 = DoMagic(input, 5, 1);
-            var test4 = DoMagic(input, 7, 1);
-            var test5 = DoMagic(input, 1, 2);
-            return (double)test1 * test2 * test3 * test4 * test5;
+            return input.OrderByDescending(t => t.Item4).First().Item4;
         }
-  
+
+        static double DoMagic2(IEnumerable<Tuple<string, int, int, int>> input)
+        {
+            bool lastRowComplete = false;
+            bool candidateRow = false;
+            int res = -1;
+            var array = input.ToArray();
+
+            for (int i = 0; i < 128; i++)
+            {
+                int numberOfSeats = input.Where(t => t.Item2 == i).Count();
+
+                if (numberOfSeats == 7 && lastRowComplete)
+                {
+                    candidateRow = true;
+                    lastRowComplete = false;
+                    res = i;
+                    continue;
+                }
+
+                if (candidateRow && numberOfSeats == 8)
+                {
+                    break;
+                }
+
+                if (numberOfSeats == 8)
+                {
+                    lastRowComplete = true;
+                }
+                else
+                {
+                    lastRowComplete = false;
+                }
+                candidateRow = false;
+                res = -1;
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                var tempId = res * 8 + i;
+                if (!input.Any(t => t.Item2 == res && t.Item4 == tempId))
+                {
+                    return tempId;
+                }
+            }
+            return -1;
+        }
+
     }
 }
